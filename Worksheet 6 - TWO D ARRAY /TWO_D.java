aa108cb249e23831e3ad4bd8ba978b4f9f3cddee
Jishnu Setia <jishnusetia8@gmail.com>
2023-11-09 00:09:36 +0400

    Commit 2

--- Worksheet 6 - TWO D ARRAY /TWO_D.java
+++ Worksheet 6 - TWO D ARRAY /TWO_D.java
@@ -1,4 +1,3 @@
-import java.util.*;
 public class TWO_D{
     int[][] M;
     int m;
@@ -37,7 +36,7 @@ public class TWO_D{
         }
     }
     public int maxnum(){
-        int maxNum=0;
+        int maxNum=M[0][0];
         for (int i=0;i<m;i++) {
             for (int j=0;j<n;j++) {
                 if (maxNum<M[i][j]){
@@ -48,10 +47,10 @@ public class TWO_D{
         return maxNum;
     }
     public int minnum(){
-        int minNum=999999999;
+        int minNum=M[0][0];
         for (int i=0;i<m;i++) {
             for (int j=0;j<n;j++) {
-                if (minNum<M[i][j]){
+                if (minNum>M[i][j]){
                     minNum=M[i][j];
                 }
             }
@@ -161,16 +160,106 @@ public class TWO_D{
         }
     }
 
-    public static void main(String[] args) {
-        int[][] a = {{1,2,3},{4,5,6},{7,8,9}};
-        TWO_D td = new TWO_D(a);
-        td.Display();
-        td.maxnum();
-        td.minnum();
-        td.search(4);
-        System.out.println(td.sumlrdiag());
-        System.out.println(td.sumrldiag());
-        System.out.println(td.diagSum());
+    public void subtract(TWO_D a, TWO_D b){
+        if(a.m==b.m&&a.n==b.n){
+            this.m=a.m;
+            this.n=a.n;
+            this.M=new int[m][n];
+            for(int i=0;i<m;i++){
+                for(int j=0;j<n;j++){
+                    this.M[i][j]+=(a.M[i][j]-b.M[i][j]);
+                }
+            }
+        }
+    }
 
+    public void multiply(TWO_D arr){
+        if (n == arr.m) {
+            int[][] temp = new int[m][arr.n];
+    
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < arr.n; j++) {
+                    for (int k = 0; k < n; k++) {
+                        temp[i][j] += M[i][k] * arr.M[k][j];
+                    }
+                }
+            }
+    
+            // Assign the values from temp back to the M array
+            M = temp;
+            n = arr.n;
+        } else {
+            System.out.println("Invalid matrix dimensions for multiplication.");
+        }
+    }
+    public static void main(String[] args) {
+        // Create an object of TWO_D class
+        TWO_D matrix = new TWO_D(3, 3);
+      
+        // Initialize the matrix
+        int[][] values = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        matrix.M = values;
+    
+        // Display the matrix
+        matrix.Display();
+    
+        // Get the maximum number in the matrix
+        int max = matrix.maxnum();
+        System.out.println("Maximum number: " + max);
+    
+        // Get the minimum number in the matrix
+        int min = matrix.minnum();
+        System.out.println("Minimum number: " + min);
+    
+        // Search for an element in the matrix
+        int searchElement = 5;
+        matrix.search(searchElement);
+    
+        // Calculate the sum of the left-to-right diagonal
+        int diagSum = matrix.diagSum();
+        System.out.println("Sum of the diagonal: " + diagSum);
+    
+        // Check if the matrix is square
+        boolean isSquare = matrix.Square();
+        System.out.println("Is the matrix square? " + isSquare);
+    
+        // Calculate the sum of the boundary elements
+        int boundarySum = matrix.sum_boundary();
+        System.out.println("Sum of the boundary elements: " + boundarySum);
+    
+        // Calculate the sum of the non-boundary elements
+        int nonBoundarySum = matrix.sum_non_boundary();
+        System.out.println("Sum of the non-boundary elements: " + nonBoundarySum);
+    
+        // Transpose the matrix
+        TWO_D transposedMatrix = matrix.trans();
+        System.out.println("Transposed matrix:");
+        transposedMatrix.Display();
+    
+        // Create a copy of the matrix
+        TWO_D copiedMatrix = matrix.copy();
+        System.out.println("Copied matrix:");
+        copiedMatrix.Display();
+    
+        // Add two matrices
+        TWO_D matrixA = new TWO_D(new int[][]{{1, 2}, {3, 4}});
+        TWO_D matrixB = new TWO_D(new int[][]{{5, 6}, {7, 8}});
+        TWO_D sumMatrix = new TWO_D();
+        sumMatrix.add(matrixA, matrixB);
+        System.out.println("Sum of matrices:");
+        sumMatrix.Display();
+    
+        // Subtract two matrices
+        TWO_D differenceMatrix = new TWO_D();
+        differenceMatrix.subtract(matrixA, matrixB);
+        System.out.println("Difference of matrices:");
+        differenceMatrix.Display();
+    
+        // Multiply two matrices
+        TWO_D matrixC = new TWO_D(new int[][]{{1, 2, 3}, {4, 5, 6}});
+        TWO_D matrixD = new TWO_D(new int[][]{{7, 8}, {9, 10}, {11, 12}});
+        matrixC.multiply(matrixD);
+        System.out.println("Product of matrices:");
+        matrixC.Display();
     }
 }
